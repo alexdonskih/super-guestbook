53d61c7190697b96b46693ce1ea255edb6df50eb
Zotoff <paulzotoff@gmail.com>
2014-08-01 14:48:26 +0400

    Made css layout

--- index.php
+++ index.php
@@ -7,14 +7,19 @@
 	<link rel="shortcut icon" href="favicon.ico" type="image/icon">
 </head>
 <body>
-	<form action="backend.php" method="GET">
-		<input type="text" name="name" placeholder="Введите имя"/><br/>
-		<input type="text" name="email" placeholder="Введите e-mail"/><br/>
-		<textarea name="msg" value="" placeholder="Введите сообщение"></textarea><br/>
-		<input type="submit"/><br/>
-		<input type="submit" name="clear" value="Очистить"/>
-		<input type="hidden" name="action" value="add_record">
-	</form>
+	<div id="guest">
+		<h2>Гостевуха</h2>
+		<form action="backend.php" method="GET">
+			<input type="text" name="name" placeholder="Введите имя"/><br/>
+			<input type="text" name="email" placeholder="Введите e-mail"/><br/>
+			<textarea rows="10" name="msg" placeholder="Введите сообщение"></textarea><br/>
+				<div class="submit_block">
+					<input class = "submit" type="submit"/>
+					<input class = "submit" type="submit" name="clear" value="Очистить"/>
+				</div>
+			<input type="hidden" name="action" value="add_record">
+		</form>
+	</div>
 <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script src="js/script.js"></script>
