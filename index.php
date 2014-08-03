58fb5f5f9e76f0ade0d9d0825e4c286dd8304b2f
Zotoff <paulzotoff@gmail.com>
2014-08-03 20:09:34 +0400

    Fixed CSS layout

--- index.php
+++ index.php
@@ -7,19 +7,21 @@
 	<link rel="shortcut icon" href="favicon.ico" type="image/icon">
 </head>
 <body>
-	<div id="guest">
-		<h2>Гостевуха</h2>
+<div id="form">
+	<h2>Гостевуха</h2>
 		<form action="backend.php" method="GET">
-			<input type="text" name="name" placeholder="Введите имя"/><br/>
-			<input type="text" name="email" placeholder="Введите e-mail"/><br/>
-			<textarea rows="10" name="msg" placeholder="Введите сообщение"></textarea><br/>
-				<div class="submit_block">
-					<input class = "submit" type="submit"/>
-					<input class = "submit" type="submit" name="clear" value="Очистить"/>
-				</div>
-			<input type="hidden" name="action" value="add_record">
+			<div class="block">
+				<input class="inp" type="text" name="name" placeholder="Введите имя"/><br/>
+				<input class="inp" type="text" name="email" placeholder="Введите e-mail"/><br/>
+				<textarea class="txt" name="msg" value="" placeholder="Введите сообщение"></textarea><br/>
+					<div class="inp2">
+						<input type="submit"/>
+						<input type="submit" name="clear" value="Очистить"/>
+					</div>
+				<input type="hidden" name="action" value="add_record">
+			</div>
 		</form>
-	</div>
+</div>
 <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script src="js/script.js"></script>
