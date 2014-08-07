77cce3982db335d932c25d2ba9a5f508f1ce9e92
Zotoff <paulzotoff@gmail.com>
2014-08-07 15:42:16 +0400

    Made simple validation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
 	// your js/jquery code here
+	document.getElementById('submit').onclick=function(event) {
+		var name_field = document.getElementById('name');
+		var email_field = document.getElementById('email');
+		var textarea = document.getElementById('msg');
+		if(name_field.value == '') {
+			alert('Заполните имя!');
+			event.preventDefault ? event.preventDefault() : (event.returnValue=false);
+		}
+		if (email_field.value == '') {
+			alert('Заполните email!');
+			event.preventDefault ? event.preventDefault() : (event.returnValue=false);
+		}
+		if (textarea.value == '') {
+			alert('Заполните текст!');
+			event.preventDefault ? event.preventDefault() : (event.returnValue=false);
+		}
+	}
 });
