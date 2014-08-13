f100f22b8c2d884d5839e821ccc9bb9c51856dc0
Zotoff <paulzotoff@gmail.com>
2014-08-13 15:52:26 +0400

    Added an array for messages and changed the messages output. Changed the comments position for more readable view

--- backend.php
+++ backend.php
@@ -4,24 +4,30 @@ switch($data['action']){
 	case 'add_record':
 
 		// задаем основные переменные
-		$name  = trim(strip_tags($data['name']));
-		$email = trim(strip_tags($data['email']));
-		$txt   = trim(strip_tags($data['msg']));
-		$redir = '<meta http-equiv="refresh" content="1; url='.$_SERVER['HTTP_REFERER'].'">';
+		$name     = trim(strip_tags($data['name']));
+		$email    = trim(strip_tags($data['email']));
+		$txt      = trim(strip_tags($data['msg']));
+		$redir    = '<meta http-equiv="refresh" content="1; url='.$_SERVER['HTTP_REFERER'].'">';
+		$messages = array (
+			'form_error' => 'Пожалуйста, заполните все формы корректно',
+			'msg_send' => 'Ваше сообщение успешно отправлено',
+			'file_write_error' => 'Ошибка записи в файл'
+		);
 
-		//проверка на пустоту, версия 1
+
+		// проверка на пустоту
 		if(empty($name) || empty($email) || empty($txt)) {
-			echo 'Пожалуйста, заполните все формы корректно';
+			echo $messages['form_error'];
 			echo $redir;
 		} else {
 			$msg = $name.' '.$email.' '.$txt."\n";
 
-			//открытие и запись файла
+		// открытие и запись файла
 			$fp = fopen('book.txt', 'a+');
 			if(fwrite($fp, $msg)){
-				echo 'Ваше сообщение успешно отправлено';
+				echo $messages['msg_send'];
 			} else {
-				echo 'Ошибка записи в файл';
+				echo $messages['file_write_error'];
 			}
 		fclose($fp);
 		echo $redir;
