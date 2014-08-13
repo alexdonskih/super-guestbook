ce83a2f6bf9cd25e346f8384cfeb44d9f0db1b07
Zotoff <paulzotoff@gmail.com>
2014-08-13 15:05:37 +0400

    Code format

--- backend.php
+++ backend.php
@@ -2,29 +2,33 @@
 $data = $_GET;
 switch($data['action']){
 	case 'add_record':
+
 		// добавляем в файл
 		$name  = trim(strip_tags($data['name']));
 		$email = trim(strip_tags($data['email']));
 		$txt   = trim(strip_tags($data['msg']));
 		$redir = '<meta http-equiv="refresh" content="1; url='.$_SERVER['HTTP_REFERER'].'">';
+
 		//проверка на пустоту, версия 1
 		if(empty($name) || empty($email) || empty($txt)) {
 			echo 'Пожалуйста, заполните все формы корректно';
 			echo $redir;
 		} else {
 			$msg = $name.' '.$email.' '.$txt."\n";
+
 			//открытие и запись файла
 			$fp = fopen('book.txt', 'a+');
-				if(fwrite($fp, $msg)){
-					echo 'Ваше сообщение успешно отправлено';
-				} else {
-					echo 'Ошибка записи в файл';
-				}
+			if(fwrite($fp, $msg)){
+				echo 'Ваше сообщение успешно отправлено';
+			} else {
+				echo 'Ошибка записи в файл';
+			}
 		fclose($fp);
 		echo $redir;
 		}
 	break;
 	case 'otheraction':
+
 		// do other stuff
 	break;
 }
